8aed12f5d3c9be0b5543473bbf2dafd541638024
Glenn Dixon <glenndixon@gmail.com>
2016-12-09 21:22:32 -0800

    derp

--- src/Dice.js
+++ src/Dice.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './Dice.css';
+import { Motion, spring } from 'react-motion';
+
+
 
 const FACES = [
   ['c'],
@@ -10,18 +13,46 @@ const FACES = [
   ['tl', 'tr', 'bl', 'br', 'l', 'r'],
 ];
 
+const TRANSFORMS = [
+  [0, 0, 0, 0],
+  [0, 1, 0, 90],
+];
+
 //TODO : https://github.com/HenrikJoreteg/create-keyframe-animation
 class Dice extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: 1,
+      random: 0
+    };
+  }
+
   render() {
     return (
-      <div className="Dice">
-        <div className="Dice__cube">
-          {FACES.map((pips, i) => <div className={`Dice__face Dice__face--${i + 1}`} key={i}>
-            {pips.map((pip, j) => <div className={`Dice__pip Dice__pip--${pip}`} key={j}></div>)}
-          </div>)}
-        </div>
+      <div className="Dice" onClick={this.blah.bind(this)}>
+        <Motion style={{x: spring(this.state.x)}}>
+          {({x}) => <div className="Dice__cube" style={{transform: `rotate3d(1, 1, 3, ${x}deg)`}}>
+              {FACES.map((pips, i) => <div className={`Dice__face Dice__face--${i + 1}`} key={i}>
+                {pips.map((pip, j) => <div className={`Dice__pip Dice__pip--${pip}`} key={j}></div>)}
+              </div>)}
+            </div>
+          }
+        </Motion>
       </div>
     );
+  }
+
+  componentWillUpdate() {
+
+  }
+
+  blah() {
+    this.setState({
+      value: Math.floor(Math.random() * 6) + 1,
+      random: Math.random()
+    });
   }
 }
 
